b8c1557d75ec3309b8db6d8a9afd530dee9930a3
louiecerv <lfcervantes@wvsu.edu.ph>
2024-04-07 18:42:01 +0800

    Save

--- Autoencoder.py
+++ Autoencoder.py
@@ -52,7 +52,7 @@ def app():
     if "dataset_ready" not in st.session_state:
         st.session_state.dataset_ready = False 
 
-    text = """Three-way comparison of ML Classifiers, MLP and Tensorflow Artificial Neural Networks on the Heart Disease Dataset"""
+    text = """Grayscale to Color: CNN Authoencoder"""
     st.header(text)
 
     text = """Prof. Louie F. Cervantes, M. Eng. (Information Engineering) \n
@@ -62,51 +62,13 @@ def app():
     West Visayas State University"""
     st.text(text)
 
-    #st.image('breast-cancer.jpg', caption="Breast Cancer Dataset")
+    #st.image('breast-cancer.jpg', caption="Replace this image")
 
-    text = """The breast cancer dataset in scikit-learn is a well-known dataset used for binary 
-        classification tasks. It contains data collected from patients diagnosed with breast cancer. 
-        Here's a breakdown of the dataset:
-        \nSource: The data consists of features extracted from digitized images of fine needle 
-        aspirates (FNA) of breast masses.
-        \nFeatures: The dataset includes 569 data points, each with 30 numerical features. 
-        These features represent various characteristics of the cell nuclei, such as radius, 
-        texture, perimeter, and area.
-        \nTarget: The target variable indicates the class, whether the tumor is malignant 
-        (cancerous) or benign (non-cancerous). There are 212 malignant cases and 357 benign cases
-    """
+    text = """Describe the image dataset"""
     st.write(text)
 
-    text = """
-    An MLP (Multi-Layer Perceptron) classifier can be used to analyze a heart disease dataset and 
-    predict whether a patient has heart disease or not. Here's how it works in this context:
-    \nBinary Classification:
-    The MLP aims for binary classification, meaning the output will be either 0 (no heart disease) or 1 (heart disease).
-    \nData Preprocessing:
-    The heart disease dataset contain various features like age, blood pressure, cholesterol levels, etc. 
-    These features might need scaling or normalization for the MLP to process them efficiently.
-    
-    \nMLP Architecture:
-    The MLP is a type of artificial neural network with an interconnected layer structure.
-    In this case, the input layer will have the size matching the number of features in the heart 
-    disease data (e.g., age, blood pressure). There will be one or more hidden layers containing 
-    a number of artificial neurons. These hidden layers extract complex patterns from the data.  
-    The final output layer will have a single neuron with a sigmoid activation function. 
-    This neuron outputs a value between 0 and 1, which is interpreted as the probability of 
-    having heart disease (closer to 1) or not (closer to 0).
-
-    \nTraining:
-    The MLP is trained using a labeled dataset where each data point has a confirmed classification 
-    (heart disease or no disease) associated with its features.
-    During training, the MLP adjusts the weights and biases between its artificial neurons to 
-    minimize the error between its predicted probabilities and the actual labels in the training data.
-    A common training algorithm for MLPs is backpropagation, which calculates the error and propagates 
-    it backward through the network to update the weights and biases.
-    \nPrediction:
-    Once trained, the MLP can predict the probability of heart disease for new, unseen data points 
-    based on their features. A threshold is typically set on the output probability (e.g., 0.5). 
-    Values above the threshold are classified as having heart disease, while those below are 
-    classified as healthy."""
+    text = """Replace with Autoencoder description
+."""
 
     st.write(text)
 
@@ -157,7 +119,6 @@ def app():
     train_c = np.reshape(train_color_image, (len(train_color_image),SIZE,SIZE,3))
     st.write('Train color image shape:',train_c.shape)
 
-
     test_gray_image = np.reshape(test_gray_image,(len(test_gray_image),SIZE,SIZE,3))
     test_color_image = np.reshape(test_color_image, (len(test_color_image),SIZE,SIZE,3))
     print('Test color image shape',test_color_image.shape)
@@ -180,7 +141,6 @@ def down(filters , kernel_size, apply_batch_normalization = True):
     downsample.add(keras.layers.LeakyReLU())
     return downsample
 
-
 def up(filters, kernel_size, dropout = False):
     upsample = tf.keras.models.Sequential()
     upsample.add(layers.Conv2DTranspose(filters, kernel_size,padding = 'same', strides = 2))
@@ -188,7 +148,6 @@ def up(filters, kernel_size, dropout = False):
         upsample.dropout(0.2)
     upsample.add(keras.layers.LeakyReLU())
     return upsample
-
 
 def model():
     inputs = layers.Input(shape= [160,160,3])
